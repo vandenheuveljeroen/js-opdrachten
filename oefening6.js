ac5dd68bf0fd7ff8e93ad008a3ad7cd0a2b18358
jeroenvandh <jeroenvandh@gmail.com>
2018-12-10 14:12:15 +0100

    oefeningen gemaakt

---
+++ oefening6.js
@@ -0,0 +1,5 @@
+let restdeling =function(){
+    let getal1=document.getElementById("getal1").value;
+    let getal2=document.getElementById("getal2").value;
+    alert(getal1%getal2);
+}
---
+++ oefening7.js
@@ -0,0 +1,6 @@
+let functie =function(){
+    let lengte=document.getElementById("getal1").value;
+    let geboortejaar=document.getElementById("getal2").value;
+    alert(((lengte*2)+5)*50-geboortejaar+1766);
+
+}
